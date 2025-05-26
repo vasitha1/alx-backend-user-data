0e6f32e994a7b3b8b08a61d7b8d369903b2b3879
vasitha1 <sulemfelsi@gmail.com>
2025-05-26 17:47:31 +0000

    Fixing file structure and adding unauthorised handler and corresponding test

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/__init__.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/__init__.py
@@ -8,4 +8,5 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
 from api.v1.views.users import *
 
+from models.user import User
 User.load_from_file()
--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify, abort
+from flask import jsonify, abort,current_app
 from api.v1.views import app_views
 
 
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'])
+def trigger_unauthorized():
+    """
+    Testing unauthorised trigger
+    """
+    abort(401)
