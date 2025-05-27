923443b0e0110913da1b104d68dd26adc831b533
vasitha1 <sulemfelsi@gmail.com>
2025-05-27 12:29:36 +0000

    Task 3: Auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,6 +8,7 @@ from typing import List, TypeVar
 
 
 class Auth:
+    """ Class to manage the API authentication """
     
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
